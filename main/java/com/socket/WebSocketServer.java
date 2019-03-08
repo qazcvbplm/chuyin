af9b4655581a08e688abda93093a45308a36dc21
tongzhuhe <tongzhuhe@lidig.com>
2019-03-08 15:43:17 +0800

    201903080958

--- main/java/com/socket/WebSocketServer.java
+++ main/java/com/socket/WebSocketServer.java
@@ -1,38 +1,39 @@
 package com.socket;
 
-import java.io.IOException;
-import java.util.concurrent.CopyOnWriteArraySet;
+import com.util.LoggerUtil;
+import org.springframework.stereotype.Component;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
-import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.springframework.stereotype.Component;
-
-import com.util.LoggerUtil;
-
-@ServerEndpoint(value = "/websocket")
+@ServerEndpoint(value = "/websocket/{id}")
 @Component
 public class WebSocketServer {
     //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
     private static int onlineCount = 0;
-    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
-    private static CopyOnWriteArraySet<WebSocketServer> webSocketSet = new CopyOnWriteArraySet<WebSocketServer>();
- 
+
+    private static Map<Integer, WebSocketServer> webSocketSet = new HashMap<>();
+
+    private int id;
+
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
-    
- 
+
     /**
      * 连接建立成功调用的方法*/
     @OnOpen
-    public void onOpen(Session session) {
+    public void onOpen(Session session, @PathParam("id") int id) {
         this.session = session;
-        webSocketSet.add(this);     //加入set中
-        addOnlineCount();           //在线数加1
+        this.id = id;
+        webSocketSet.put(id, this);     //加入set中
+        addOnlineCount();                //在线数加1
         try {
         	 sendMessage("连接成功");
         } catch (IOException e) {
@@ -45,19 +46,12 @@ public class WebSocketServer {
      */
     @OnClose
     public void onClose() {
-        webSocketSet.remove(this);  //从set中删除
+        webSocketSet.remove(this.id);  //从set中删除
         subOnlineCount();           //在线数减1
     }
- 
+
+
     /**
-     * 收到客户端消息后调用的方法
-     *
-     * @param message 客户端发送过来的消息*/
-    @OnMessage
-    public void onMessage(String message, Session session) {
-    }
- 
-	/**
 	 * 
 	 * @param session
 	 * @param error
